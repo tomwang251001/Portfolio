10c3466106672d580a196b72c366452416e01024
tomwang251001 <114426322+tomwang251001@users.noreply.github.com>
2024-03-17 19:33:18 +0100

    first commit

---
+++ buttons.js
@@ -0,0 +1,14 @@
+function about(){
+    console.log('hi');
+document.querySelector('.card').style.height = '85vh';
+document.querySelector('.card').style.opacity = '1';
+}
+
+function about_close(){
+    console.log('hi');
+document.querySelector('.card').style.height = '0vh';
+document.querySelector('.card').style.opacity = '0';
+}
+
+function linkedin(){window.location.href='https://www.linkedin.com/in/tom-wang-b89403293/';}
+function github(){window.location.href='https://github.com/';}
